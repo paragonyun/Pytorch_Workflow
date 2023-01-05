c9936673a1d8e96b9692385e499e8b259a2c8ff9
paragonyun <paragonyun@naver.com>
2023-01-05 21:21:20 +0900

    MAIN

--- 딥러닝_파이토치_교과서/Chapter13/main.py
+++ 딥러닝_파이토치_교과서/Chapter13/main.py
@@ -5,7 +5,9 @@ import torch.optim as optim
 
 from GAN_dataset import return_dataloader
 from GAN_model import Discriminator, Generator
+from train import GANTrainer
 
+train_loader = return_dataloader()
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -16,3 +18,14 @@ optim_g = optim.Adam(generator.parameters(), lr=0.0002)
 optim_d = optim.Adam(discriminator.parameters(), lr=0.0002)
 
 criterion = nn.BCELoss()
+
+gan = GANTrainer(
+    discriminator=discriminator,
+    generator=generator,
+    criterion=criterion,
+    optim_g=optim_g,
+    optim_d=optim_d,
+    epochs=100,
+    train_loader=train_loader,
+)
+gan.trainer()
